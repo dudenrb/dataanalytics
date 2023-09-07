d9562fd9b6315febf09a118f659b3c99a28bf94f
Nikhil Raj <94313877+dudenrb@users.noreply.github.com>
2023-09-07 15:51:22 +0530

    Add files via upload

---
+++ project.sql
@@ -0,0 +1,10 @@
+-- create database if not exist RetailSalesData;
+use RetailSalesData;
+-- create table if not exist Sales_Data_Transactions (
+-- customer_id varchar(255) primary key,
+-- trans_date varchar(255),
+-- trans_amount int);
+-- create table if not exist Sales_Data_Response (
+-- customer_id varchar(255) primary key,
+-- response int);
+select * from Sales_Data_Transactions limit 10;
